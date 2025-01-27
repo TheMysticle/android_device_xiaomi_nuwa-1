357fa0626d70ed91fd4fc878bf9c43cd3ac8eef8
SkyMinus <minus_rav@qq.com>
2025-01-27 20:07:57 +0800

    nuwa: Set permissions for touch sensors
    Change-Id: I777ed71ecc5dc502182567bc7e1166605251af93
    Signed-off-by: SkyMinus <minus_rav@qq.com>

--- device.mk
+++ device.mk
@@ -175,6 +175,7 @@ PRODUCT_PACKAGES += \
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.sm8550.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.sm8550.rc \
+    $(LOCAL_PATH)/init/init.nuwa.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.nuwa.rc \
     $(LOCAL_PATH)/init/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
 
 # IPA
