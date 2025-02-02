ffb6ddf8a323bbd40c8231ca1649c25471d3ff8b
TheMysticle <36317711+TheMysticle@users.noreply.github.com>
2025-02-02 19:39:52 +0100

    Add hotword enrollment blobs

--- device.mk
+++ device.mk
@@ -112,6 +112,10 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti \
     android.hardware.boot@1.2-impl-qti.recovery
 
+# Hotword Enrollement
+PRODUCT_COPY_FILES += \
+		$(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.concurrent.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
